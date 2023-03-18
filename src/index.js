c954d52503ee1ff6faa7de585776401bc3af3912
xerpa43 <angngima50@gmail.com>
2023-03-18 11:33:07 +0545

    changed the color

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App'
 import './index.css';
+document.querySelector('img[src="https://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png"]').style.display = 'none';
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />)
