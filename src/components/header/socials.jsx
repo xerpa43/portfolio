00907dd2b96f1f668d820b844ed945aecac7f757
Ang Ngima Lama Sherpa <89342874+xerpa43@users.noreply.github.com>
2024-11-14 19:52:57 +0545

    Update socials.jsx

--- src/components/header/socials.jsx
+++ src/components/header/socials.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { FaGithub, FaFacebook, FaLinkedin } from 'react-icons/fa'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 const socials = () => {
   return (
     <>
       <div className="header-socials">
-        <a target='_blank' href="https://www.facebook.com/angngima.sherpa.52/"><FaFacebook /></a>
         <a target='_blank' href="https://github.com/xerpa43"><FaGithub /></a>
         <a target='_blank' href="https://www.linkedin.com/in/xerpa43/"><FaLinkedin /></a>
       </div>
